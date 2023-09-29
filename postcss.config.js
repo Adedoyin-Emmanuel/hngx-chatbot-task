50b00ebd357e61ca67a4d57c4f1247b9ecef3692
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-29 23:22:25 +0100

    Added tailwind

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-undef
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
+};
